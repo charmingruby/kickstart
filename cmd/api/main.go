b86aa39224b2dfa6886045b2828c80c9ba3a91d8
charmingruby <mrrubycanal3105@gmail.com>
2024-06-05 12:27:07 -0300

    feat: environment config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+
+	"github.com/charmingruby/kickstart/config"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	println("hello world")
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	if err := godotenv.Load(); err != nil {
+		slog.Warn(".env file not found")
+	}
+
+	_, err := config.NewConfig()
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+
 }
---
+++ doc/setup.md
@@ -0,0 +1,16 @@
+# Setup
+
+## 1 - Init your own go module
+
+    1. Remove initial go module
+    2. Initialize your own
+    3. Replace all old ocurrencies with the new name
+
+## 2 - Docker
+
+    Change names
+
+## 3 - Environment
+
+    1. Create .env file with needed variables
+    2. Change on ./config/config.go
