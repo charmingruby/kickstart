0bf73749075f2f3d61c4f99a0b5db63473b7c0d5
charmingruby <mrrubycanal3105@gmail.com>
2024-06-06 11:12:19 -0300

    feat: rest server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,7 +8,9 @@ import (
 	"github.com/charmingruby/kickstart/config"
 	"github.com/charmingruby/kickstart/internal/database"
 	"github.com/charmingruby/kickstart/internal/domain/example"
+	"github.com/charmingruby/kickstart/internal/transport/rest"
 	"github.com/charmingruby/kickstart/pkg/postgres"
+	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 )
@@ -34,12 +36,19 @@ func main() {
 	}
 
 	initDependencies(db)
+
+	router := gin.Default()
+	server := rest.NewServer(router, cfg.ServerConfig.Port)
+	if err := server.Start(); err != nil {
+		slog.Error(fmt.Sprintf("REST SERVER: %s", err.Error()))
+		os.Exit(1)
+	}
 }
 
 func initDependencies(db *sqlx.DB) {
 	exampleRepo, err := database.NewPostgresExampleRepository(db)
 	if err != nil {
-		slog.Error(fmt.Sprintf("POSTGRES REPOSITORY: %s", err.Error()))
+		slog.Error(fmt.Sprintf("DATABASE REPOSITORY: %s", err.Error()))
 		os.Exit(1)
 	}
 
